8bd8d0693f868e1f18f0abe5f2ea89e2d35b88eb
Ron Rejwan <rejwan1@gmail.com>
2015-01-28 14:17:16 +0200

    Added some documentation

--- Assets/Scripts/Coroutines/Yields/JBCoroutineYielder.cs
+++ Assets/Scripts/Coroutines/Yields/JBCoroutineYielder.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿/*************************************************************************
+ * 
+ *  Jelly Coroutines 
+ *  By Ron Rejwan (Jan 2015) - ronr@jellybtn.com
+ *  Jelly Button Games LTD. (http://www.jellybtn.com)
+ *  -----------------------------------------------------------------
+ *
+ *  Feel free to use these coroutines in your project, please just leave
+ *  these comments in the file and let me know if I helped you out.
+ *  
+ *  Also make sure to commit any fixes/improvement to our repository:
+ *  https://github.com/rejwan/Jelly-Coroutines
+ * 
+ *************************************************************************/
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using JellyTools.Coroutines.Interfaces;
